6f1963cc77519475b80d9229b2532b94b3ff41a4
Bato <bato19941994@bk.ru>
2023-12-04 19:15:04 +0800

    updateFormTest

--- src/test/java/ru/netology/web/FormTest.java
+++ src/test/java/ru/netology/web/FormTest.java
@@ -1,14 +1,17 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.selector.WithText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import org.openqa.selenium.Keys;
 
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -17,24 +20,27 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FormTest {
-    java.util.Date date = new java.util.Date();
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
-
-
+    String planningDate = generateDate(4);
 
     @Test
     void shouldCompleteForm() throws InstantiationException {
         open("http://localhost:9999");
 
-
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").setValue(String.valueOf(date));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Васильев Василий");
         $("[data-test-id='phone'] input").setValue("+79270000000");
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-     //   $("[data-test-id=\"order-success\"]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
 
     }
